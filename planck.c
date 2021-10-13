5cdb10da70df42f320fd0082207f60372f744351
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2021-10-13 23:02:31 +0800

    Create planck.c

---
+++ planck.c
@@ -0,0 +1,18 @@
+/*
+File : planck.c
+Author : Wu Yuqing
+ID : 191250151
+Date : 2021 - 10 - 13
+*/
+
+// h = 6.62607015e-34
+#include <stdio.h>
+
+int main(){
+
+    double v;
+    scanf("%lf",&v);//input v
+    printf("Hello world\n");
+
+    return 0;
+}
