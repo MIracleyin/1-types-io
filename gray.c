b88bada43bfddad8b634efee4fae0ebac64285eb
Labelray <labelray@icloud.com>
2021-10-13 20:03:09 +0800

    +gray.c

---
+++ gray.c
@@ -0,0 +1,20 @@
+/**
+ * File: weekday.c
+ *
+ * Author:
+ * ID:
+ * Date:
+ *
+ * This template is created by Boying Li (李博颖) (10-13-2021)
+ */
+
+#include <stdio.h>
+
+int main() {
+//  input example: 10
+//  output example: 1111
+  int n;
+  scanf("%d", &n);
+
+  return 0;
+}
