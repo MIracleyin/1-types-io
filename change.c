4eb82768e63f338d44f55dd913686ed043c0b10b
CzxingcHen <42157985+CzxingcHen@users.noreply.github.com>
2021-10-13 19:38:12 +0800

    +change.c

---
+++ change.c
@@ -0,0 +1,19 @@
+/**
+ * File: change.c
+ *
+ * Author:
+ * ID:
+ * Date:
+ *
+ * This template is created by ant-hengxin (10-13-2021)
+ */
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  printf("Hello World!\n");
+
+  return 0;
+}
+
